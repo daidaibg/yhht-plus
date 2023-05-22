c8a762c1193bac6f3a348168ac761047da5eb628
刘嘉威 <daidaibg@163.com>
2023-05-22 16:43:09 +0800

    修改logo

---
+++ assets/anchor-23a6c55f.js
@@ -0,0 +1,135 @@
+import{d as f,r as d,m as _,k as e,q as n,h as c,u as t,a4 as m,a5 as v,F as g,a1 as y,ab as b,X as x,Y as A,e as k,ad as l,ae as o,_ as S}from"./index-636e6187.js";const P=`<template>
+  <div class="anchor-demo">
+    <yh-anchor container="body" :bounds="50" :targetOffset="120">
+      <template #cursor>
+        <div class="test-cursor" />
+      </template>
+      <yh-anchor-item href="#基础锚点" title="基础锚点" />
+      <yh-anchor-item href="#自定义游标锚点" title="自定义游标锚点" />
+      <yh-anchor-item href="#注意" title="注意" />
+      <yh-anchor-item href="#Anchor-Props" title="Anchor Props" />
+    </yh-anchor>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.anchor-demo {
+  border: 1px solid transparent;
+  padding: 20px;
+  margin: -20px;
+
+  .test-cursor {
+    width: 10px;
+    height: 10px;
+    background-color: #018E9A;
+    position: absolute;
+    border-radius: 50%;
+    left: 50%;
+    margin-left: -5px;
+    top: 50%;
+    margin-top: -5px;
+  }
+}
+</style>`,R=`<template>
+    <div id="container" class="anchor-demo anchor-container-demo">
+        <yh-anchor id="default" container="#anchor-container" no-link>
+            <yh-anchor-item href="#content-1" title="content-1" />
+            <yh-anchor-item href="#content-2" title="content-2" />
+            <yh-anchor-item href="#content-3" title="content-3" />
+            <yh-anchor-item href="#content-4" title="content-4" />
+        </yh-anchor>
+        <div id="anchor-container">
+            <div id="content-1" class="anchor-content-1" style="">anchor-content-1</div>
+            <div id="content-2" class="anchor-content-2">anchor-content-2</div>
+            <div id="content-3" class="anchor-content-3">anchor-content-3</div>
+            <div id="content-4" class="anchor-content-4">anchor-content-4</div>
+            <div id="content-5" class="anchor-content-5">anchor-content-5</div>
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+.anchor-container-demo {
+  display: flex;
+}
+#anchor-container {
+  flex-grow: 1;
+  height: 200px;
+  overflow: auto;
+  > div {
+    text-align: center;
+    font-size: 22px;
+    line-height: 100px;
+  }
+  .anchor-content-1 {
+    background: #ecf2fe;
+  }
+  .anchor-content-2 {
+    background: #d4e3fc;
+  }
+  .anchor-content-3 {
+    background: #bbd3fb;
+  }
+  .anchor-content-4 {
+    background: #96bbf8;
+  }
+  .anchor-content-5 {
+    background: #699ef5;
+  }
+}
+</style>`,w=`\r
+\r
+### 注意\r
+\r
+!!! warning \r
+\r
+锚点链接, 如果是 hash路由 请按照以下方式,传入\`hashRouter\`参数\r
+\r
+!!!\r
+\r
+\`\`\`vue\r
+<script setup lang="ts"> \r
+    import { useRoute } from "vue-router";\r
+    const route = useRoute();\r
+<\/script> \r
+\r
+<template> \r
+    <yh-anchor-item \r
+        href="#default" \r
+        title='基础锚点' 	\r
+        :hashRouter="route.path"\r
+    /> \r
+</template>\r
+\`\`\`\r
+### Anchor Props\r
+\r
+| 参数          | 说明                                                         | 类型              | 默认值               | 可选值                     |\r
+| ------------- | ------------------------------------------------------------ | ----------------- | -------------------- | -------------------------- |\r
+| bounds        | 锚点区域边界                                                 | Number            | 5                    | -                          |\r
+| container     | 指定滚动的容器。数据类型为 String 时，会被当作选择器处理，进行节点查询。示例：'body' 或 () => document.body。TS 类型：ScrollContainer | String 、 Function | () => (() => window) | -                          |\r
+| currentActive | 当前锚点，首次进入页面的锚点需要传入                         | String            |                      | -                          |\r
+| size          | 组件尺寸                                                     | String            | medium               | 可选项：small、medium、large |\r
+| targetOffset  | 锚点滚动偏移量                                               | Number            | 0                    | -                          |\r
+| cursor        | 用于自定义选中项左侧游标。                                   | Slot              | -                    | -                          |\r
+| noLink        | 不在当前路径张增加参数                                       | Boolean           | false                | -                          |\r
+| hashRouter    | 如果是hash路由 词参数必填，上面注意情况                      | String 、 Slot     |                      | -                          |\r
+\r
+### Anchor Events\r
+\r
+| 事件名称 | 参数                                                   | 说明             | 备注                                     |\r
+| -------- | ------------------------------------------------------ | ---------------- | ---------------------------------------- |\r
+| change   | (currentLink: string, prevLink: string)                | 锚点区域边界     | currentLink 当前连接，prevLink下一个链接 |\r
+| click    | (link: { href: string; title: string; e: MouseEvent }) | 锚点被点击时触发 |                                          |\r
+\r
+### AnchorItem Props\r
+\r
+| 参数   | 说明                                              | 类型          | 默认值 | 可选值                    |\r
+| ------ | ------------------------------------------------- | ------------- | ------ | ------------------------- |\r
+| href   | 必需。锚点链接, 如果是 hash 模式需要加上当前 path | String        | -      | -                         |\r
+| target | 锚点文本。可选项：_self、_blank、_parent、_top       | String        | _self  | \`_self\`、\`_blank\`、\`_parent\`、\`_top\` |\r
+| title  | 锚点文本                                          | String 、 Slot |        | -                         |`,r=s=>(x("data-v-fe352e6f"),s=s(),A(),s),T={class:"buttons"},I=r(()=>e("h2",{class:"yh-title",id:"Anchor锚点"},"Anchor 锚点",-1)),C=r(()=>e("p",null,"页面内的超级链接，用于跳转到页面内指定位置",-1)),E=r(()=>e("h3",{class:"yh-title",id:"基础锚点"},"基础锚点 ",-1)),L=r(()=>e("h3",{class:"yh-title",id:"自定义游标锚点"},"自定义游标锚点",-1)),N=r(()=>e("p",null,"targetOffset 锚点滚动偏移量 ",-1)),B={class:"anchor-demo"},O=r(()=>e("div",{class:"test-cursor"},null,-1)),F=r(()=>e("h3",{class:"yh-title",id:"指定容器锚点"},"指定容器锚点",-1)),z=r(()=>e("p",null,"指定响应滚动的容器进行锚点定位",-1)),V=r(()=>e("div",{class:"warning"},[e("p",null,"如果使用了指定容器配置container,如果您的元素充满整个html不用考虑以下，根据业务需求进行调整配置。使用no-link配置可以不把锚点拼接到url后边，拼接到路径后边会使指定容器也置顶。")],-1)),q={id:"container",class:"anchor-demo anchor-container-demo"},M=r(()=>e("div",{id:"anchor-container"},[e("div",{id:"content-1",class:"anchor-content-1",style:{}},"anchor-content-1"),e("div",{id:"content-2",class:"anchor-content-2"},"anchor-content-2"),e("div",{id:"content-3",class:"anchor-content-3"},"anchor-content-3"),e("div",{id:"content-4",class:"anchor-content-4"},"anchor-content-4"),e("div",{id:"content-5",class:"anchor-content-5"},"anchor-content-5")],-1)),X=f({__name:"anchor",setup(s){const i=d(""),a=y();a.hash&&a.hash!==""&&(i.value=a.hash);const u=`<yh-anchor container="body" :bounds="50" >
+  <yh-anchor-item href="#基础锚点" title="基础锚点" />
+  <yh-anchor-item href="#自定义游标锚点" title="自定义游标锚点" />
+  <yh-anchor-item href="#注意" title="注意" />
+  <yh-anchor-item href="#Anchor-Props" title="Anchor Props" />
+</yh-anchor>`,p=d([{href:"基础锚点",title:"基础锚点"},{href:"自定义游标锚点",title:"自定义游标锚点"},{href:"指定容器锚点",title:"指定容器锚点"},{href:"注意",title:"注意"},{href:"Anchor-Props",title:"Anchor Props"},{href:"Anchor-Events",title:"Anchor Events"},{href:"AnchorItem-Props",title:"AnchorItem Props"}]);return(Y,$)=>{const h=b;return k(),_(g,null,[e("div",T,[I,C,E,n(h,{codeText:u,style:{position:"relative"},"code-type":"language-xml vue"},{default:c(()=>[n(t(l),{container:"body",bounds:50,currentActive:i.value,hashRouter:t(a).path},{default:c(()=>[n(t(o),{href:"#基础锚点",title:"基础锚点"}),n(t(o),{href:"#自定义游标锚点",title:"自定义游标锚点"}),n(t(o),{href:"#注意",title:"注意"}),n(t(o),{href:"#Anchor-Props",title:"Anchor Props"})]),_:1},8,["currentActive","hashRouter"])]),_:1}),L,N,n(h,{codeText:t(P),style:{position:"relative"},"code-type":"language-xml vue"},{default:c(()=>[e("div",B,[n(t(l),{container:"body",bounds:50,targetOffset:120,currentActive:i.value,hashRouter:t(a).path},{cursor:c(()=>[O]),default:c(()=>[n(t(o),{href:"#基础锚点",title:"基础锚点"}),n(t(o),{href:"#自定义游标锚点",title:"自定义游标锚点"}),n(t(o),{href:"#注意",title:"注意"}),n(t(o),{href:"#Anchor-Props",title:"Anchor Props"})]),_:1},8,["currentActive","hashRouter"])])]),_:1},8,["codeText"]),F,z,V,n(h,{codeText:t(R),style:{position:"relative"},"code-type":"language-xml vue"},{default:c(()=>[e("div",q,[n(t(l),{id:"default",container:"#anchor-container","no-link":"",hashRouter:t(a).path},{default:c(()=>[n(t(o),{href:"#content-1",title:"content-1"}),n(t(o),{href:"#content-2",title:"content-2"}),n(t(o),{href:"#content-3",title:"content-3"}),n(t(o),{href:"#content-4",title:"content-4"})]),_:1},8,["hashRouter"]),M])]),_:1},8,["codeText"]),n(m,{text:t(w)},null,8,["text"])]),n(v,{list:p.value,isNoTranslate:""},null,8,["list"])],64)}}});const D=S(X,[["__scopeId","data-v-fe352e6f"]]);export{D as default};
