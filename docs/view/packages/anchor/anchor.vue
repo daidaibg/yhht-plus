e9fe932749eb4a82d0cc97a8dca030ca1e561cf8
刘嘉威 <daidaibg@163.com>
2023-07-12 14:37:29 +0800

    perf: 锚点测试用例增加一个模块内

--- docs/view/packages/anchor/anchor.vue
+++ docs/view/packages/anchor/anchor.vue
@@ -6,6 +6,29 @@ const currentActive = ref("");
 </script>
 
 <template>
+  <div id="container" class="anchor-demo anchor-container-demo">
+    <yh-anchor
+      id="default"
+      container="#anchor-container"
+      no-link
+      :hashRouter="route.path"
+      :targetOffset="0"
+    >
+      <yh-anchor-item href="#content-1" title="content-1" />
+      <yh-anchor-item href="#content-2" title="content-2" />
+      <yh-anchor-item href="#content-3" title="content-3" />
+      <yh-anchor-item href="#content-4" title="content-4" />
+    </yh-anchor>
+    <div id="anchor-container">
+      <div id="content-1" class="anchor-content-1" style="">
+        anchor-content-1
+      </div>
+      <div id="content-2" class="anchor-content-2">anchor-content-2</div>
+      <div id="content-3" class="anchor-content-3">anchor-content-3</div>
+      <div id="content-4" class="anchor-content-4">anchor-content-4</div>
+      <div id="content-5" class="anchor-content-5">anchor-content-5</div>
+    </div>
+  </div>
   <div class="anchor-test">
     <div id="基础锚点" style="height: 500px">
       <h2>基础锚点</h2>
@@ -43,9 +66,33 @@ const currentActive = ref("");
 </template>
 
 <style scoped lang="scss">
-.anchor-test {
-  width: 100%;
-  height: 100%;
-  overflow-y: auto;
+
+.anchor-container-demo {
+  display: flex;
+}
+#anchor-container {
+  flex-grow: 1;
+  height: 200px;
+  overflow: auto;
+  > div {
+    text-align: center;
+    font-size: 22px;
+    line-height: 100px;
+  }
+  .anchor-content-1 {
+    background: #ecf2fe;
+  }
+  .anchor-content-2 {
+    background: #d4e3fc;
+  }
+  .anchor-content-3 {
+    background: #bbd3fb;
+  }
+  .anchor-content-4 {
+    background: #96bbf8;
+  }
+  .anchor-content-5 {
+    background: #699ef5;
+  }
 }
 </style>
