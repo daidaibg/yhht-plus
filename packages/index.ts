cf340d6bcf6277adb6eb44642b88979f175609cf
刘嘉威 <daidaibg@163.com>
2024-04-02 15:12:28 +0800

    perf: 类型错误修改

--- packages/index.ts
+++ packages/index.ts
@@ -14,7 +14,7 @@ const components = [
 // 定义 install 方法， App 作为参数
 const install = (app: App): void => {
   // 遍历注册所有组件
-  components.map((component) => app.component(component.name, component))
+  components.map((component) => app.component(component.name as string, component))
 }
 
 
