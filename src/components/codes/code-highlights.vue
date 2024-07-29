91a94fc44070a9d73b349588c0d46ce79af7ae70
daidai <daidaibg@163.com>
2024-07-29 15:34:28 +0800

    fix: 代码解析错误问题

--- src/components/codes/code-highlights.vue
+++ src/components/codes/code-highlights.vue
@@ -1,70 +1,70 @@
-  <script lang="ts" setup>
-  import { watch, computed, reactive, ref } from "vue";
-  import { MdPreview } from "md-editor-v3";
-  import { userThemeStore } from "@/store";
-  import "md-editor-v3/lib/preview.css";
+<script lang="ts" setup>
+import { watch, computed, reactive, ref } from "vue";
+import { MdPreview } from "md-editor-v3";
+import { userThemeStore } from "@/store";
+import "md-editor-v3/lib/preview.css";
 
-  import type { PropType } from "vue";
+import type { PropType } from "vue";
 
-  const themeStore = userThemeStore();
+const themeStore = userThemeStore();
 
-  const props = defineProps({
-    text: {
-      type: String,
-      default: () => "",
-    },
-    wrapClass: {
-      type: String,
-      default: () => "",
-    },
-    codeType: {
-      type: String,
-      default: () => "",
-    },
-    asyncCodeText: {
-      type: Function as PropType<() => Promise<string>> | undefined,
-      default: void 0,
-    },
-    codeFoldable: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  text: {
+    type: String,
+    default: () => "",
+  },
+  wrapClass: {
+    type: String,
+    default: () => "",
+  },
+  codeType: {
+    type: String,
+    default: () => "",
+  },
+  asyncCodeText: {
+    type: Function as PropType<() => Promise<string>> | undefined,
+    default: void 0,
+  },
+  codeFoldable: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const str = ref("");
+
+if (props.asyncCodeText) {
+  props.asyncCodeText().then((res) => {
+    str.value = res;
   });
+}
 
-  const str = ref("");
-
-  if (props.asyncCodeText) {
-    props.asyncCodeText().then((res) => {
-      str.value = res;
-    });
+const codeText = computed(() => {
+  console.log({ codeType: props.codeType, asyncCodeText: props.asyncCodeText, str: str.value, text: props.text });
+  return `
+\`\`\`${props.codeType}
+${props.asyncCodeText ? str.value : props.text}
+\`\`\`
+`;
+});
+watch(
+  () => codeText.value,
+  (val) => {
+    console.log(val);
   }
+);
+</script>
 
-  const codeText = computed(() => {
-    console.log({ codeType: props.codeType, asyncCodeText: props.asyncCodeText, str: str.value, text: props.text });
-    return `
-    \`\`\`${props.codeType}
-    ${props.asyncCodeText ? str.value : props.text}
-    \`\`\`
-          `;
-  });
-  watch(
-    () => codeText.value,
-    (val) => {
-      console.log(val);
-    }
-  );
-  </script>
+<template>
+  <MdPreview
+    :model-value="codeText"
+    :theme="themeStore.theme"
+    :previewTheme="themeStore.previewTheme"
+    :code-theme="themeStore.codeTheme"
+    showCodeRowNumber
+    :codeFoldable="codeFoldable"
+    id="yh-md"
+  />
+</template>
 
-  <template>
-    <MdPreview
-      :model-value="codeText"
-      :theme="themeStore.theme"
-      :previewTheme="themeStore.previewTheme"
-      :code-theme="themeStore.codeTheme"
-      showCodeRowNumber
-      :codeFoldable="codeFoldable"
-      id="yh-md"
-    />
-  </template>
-
-  <style lang="scss" scoped></style>
+<style lang="scss" scoped></style>
