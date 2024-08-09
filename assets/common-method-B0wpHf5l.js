67fbde1ee1a04c8918d709bb96eb9aac8e606eb1
daidaibg <55817318+daidaibg@users.noreply.github.com>
2024-08-09 02:32:57 +0000

    Deploying to pages from @ daidaibg/yhht-plus@acb52241a855da242e773f6d4f457bdbb46bc29c 🚀

---
+++ assets/common-method-B0wpHf5l.js
@@ -0,0 +1,131 @@
+import{_ as r}from"./marked.vue_vue_type_script_setup_true_lang-BtZdUkQz.js";import{d as n,h as o,u as t,f as e}from"./index-YqLftI8U.js";import"./right-anchor-DWwzJc_D.js";const l=`## on(绑定事件)\r
+\r
+同原生的\`addEventListener\`只是简单做了优化\r
+\r
+\`\`\`js\r
+import { on } from "yhht-plus/lib/utils"\r
+\r
+const onScroll=(e)=>{\r
+    \r
+}\r
+\r
+on(document.querySelector("body"), "scroll", onScroll);\r
+\`\`\`\r
+\r
+## off(移除事件)\r
+\r
+同原生的\`removeEventListener\`只是简单做了优化\r
+\r
+!!! warning \r
+\r
+移除的函数要与on绑定函数是同一个\r
+\r
+!!!\r
+\r
+\`\`\`js\r
+import { off } from "yhht-plus/lib/utils"\r
+\r
+const onScroll=(e)=>{\r
+    \r
+}\r
+off(document.querySelector("body"), "scroll", onScroll);\r
+\`\`\`\r
+\r
+ ## getScroll\r
+\r
+\r
+\r
+\`\`\`js\r
+/**\r
+ * 获取滚动条目前置顶距离\r
+ * @param {ScrollTarget} target\r
+ * @param {boolean} isLeft true为获取scrollLeft, false为获取scrollTop\r
+ * @returns {number}\r
+ */\r
+ getScroll(target: ScrollTarget, isLeft?: boolean): number\r
+ \r
+ \r
+ //例子\r
+import { getScroll } from "yhht-plus/lib/utils"\r
+\r
+const top = getScroll(document.querySelector("body"))\r
+const left = getScroll(document.querySelector("body"),true)\r
+\`\`\`\r
+\r
+ \r
+\r
+##  scrollTo\r
+\r
+让滚动条滚动到指定位置,返回\`Promise\`\r
+\r
+**ScrollTopOptions**配置\r
+\r
+- \`container\`    ----   dom元素对象\r
+- \`duration\`      ----    时间\r
+- \`container\`    ----   动画\r
+\r
+\r
+\r
+\`\`\`ts\r
+interface ScrollTopOptions {\r
+    container?: ScrollTarget;\r
+    duration?: number;  \r
+    easing?: EasingFunction;\r
+}\r
+\r
+function scrollTo(target: number, opt: ScrollTopOptions): Promise<ScrollToResult>\r
+\`\`\`\r
+\r
+例子\r
+\r
+\`\`\`js\r
+import { scrollTo } from "yhht-plus/lib/utils"\r
+\r
+scrollTo(100, {\r
+    container: document.querySelector("body"),\r
+});\r
+\`\`\`\r
+\r
+\r
+\r
+##  getScrollContainer\r
+\r
+\r
+\r
+ 获取滚动容器,因为document不存在scroll等属性, 因此排除document\r
+\r
+\`\`\`js\r
+/**\r
+* 获取滚动容器\r
+* 因为document不存在scroll等属性, 因此排除document\r
+* window | HTMLElement\r
+* @param {ScrollContainerElement} [container='body']  可传入方法\r
+* @returns {ScrollContainer}\r
+*/\r
+getScrollContainer = (container: ScrollContainer = 'body'): ScrollContainerElement \r
+\`\`\`\r
+\r
+!!! warning \r
+\r
+因为内部采用\`querySelector\` 获取元素对象，获取id时候i请带\`#\`\r
+\r
+!!!\r
+\r
+例子：\r
+\r
+\`\`\`js\r
+import { getScrollContainer } from "yhht-plus/lib/utils"\r
+\r
+const dom1 = getScrollContainer(".test")\r
+const dom2 = getScrollContainer("#test")\r
+const dom3 = getScrollContainer("div")\r
+\r
+function dom4Fun(){\r
+\r
+}\r
+const dom4 = getScrollContainer(dom4Fun)\r
+\r
+const dom4 = getScrollContainer(document.querySelector("body"))\r
+\`\`\`\r
+\r
+`,d=n({__name:"common-method",setup(c){return(s,i)=>(e(),o(r,{text:t(l),isAnchor:""},null,8,["text"]))}});export{d as default};
---
+++ assets/contextMenuDirective-DXCSXRxE.js
@@ -0,0 +1,36 @@
+const n=`<template>\r
+  <yh-button v-context-menu="options">Dom</yh-button>\r
+</template>\r
+<script setup lang="ts">\r
+import { onMounted, ref } from "vue";\r
+import { ContextMenuDirective as vContextMenu } from "yhht-plus";\r
+import type { CustomMouseMenuOptions } from "yhht-plus/lib/components/contextmenu/index";\r
+\r
+const options: CustomMouseMenuOptions = {\r
+  params: { a: 1 },\r
+  menuList: [\r
+    {\r
+      label: "打开",\r
+      tips: "Open",\r
+      fn: (params, currentEl, bindingEl, e) => {\r
+        console.log("open", params, currentEl, bindingEl, e);\r
+      },\r
+    },\r
+    {\r
+      label: "重命名",\r
+      fn: (params, currentEl, bindingEl, e) => {\r
+        console.log("rename", params, currentEl, bindingEl, e);\r
+      },\r
+    },\r
+  ],\r
+  // 菜单Open回调\r
+  onOpen: (params, currentEl, bindingEl) => {\r
+    console.log("onOpen", params, currentEl, bindingEl);\r
+  },\r
+  // 菜单Close回调\r
+  onClose: (params, currentEl, bindingEl) => {\r
+    console.log("onClose", params, currentEl, bindingEl);\r
+  },\r
+};\r
+<\/script>\r
+`;export{n as default};
